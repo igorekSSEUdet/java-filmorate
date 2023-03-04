e53a8e29b7ff97eaef273066509c23c06be8ffe5
igorekSSEUdet <korablev_sseu@mail.ru>
2023-03-04 18:50:06 +0400

    merge

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -13,7 +13,7 @@ public class Film {
     private int id;
     @NotBlank(message = "The title of the movie cannot be empty")
     private final String name;
-    @Size(min = 0, max = 200, message = "The maximum length of the description is 200 characters")
+    @Size(max = 200, message = "The maximum length of the description is 200 characters")
     private final String description;
     @CheckIfBefore1895
     private final LocalDate releaseDate;
